da5d8994b9d5f613fd1668bd0b48ff84ce217ba9
Maximilian Schoening <resetability@gmail.com>
2010-10-15 16:02:29 +0200

    Updated path to release notes

--- main.rb
+++ main.rb
@@ -73,7 +73,7 @@ end
 get '/releases/:release' do
   response.headers['Cache-Control'] = 'public, max-age=31557600'
   
-  File.read("public/#{ params[:release] }index.html")
+  File.read("public/#{ params[:release] }/index.html")
 end
 
 get '/admin' do
